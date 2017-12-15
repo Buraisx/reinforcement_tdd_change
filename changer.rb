9a11877a46c9f54f3e6b706329037e057721254d
Situ William <situ_william@yahoo.com>
2017-12-15 16:22:40 -0500

    refactored some code

--- changer.rb
+++ changer.rb
@@ -1,14 +1,6 @@
 class Changer
-	@@money = 0
-	@@change = []
 	def self.make_change(number)
 	  	# change has to come in a combination of quarters, dimes, nickels, pennies
-	  	# biggest change
-	  	# 25 - 25 = 0
-	  	# 50 - 25 - 25 = 0
-	  	# 10 - 25 = -    10 - 10 = 0
-	  	# 20 - 25 = - / 20 - 10 - 10 = 0
-
 	  	@@money = number
 	  	@@change =[]
 	  	all_coins = [25,10,5,1]
@@ -18,9 +10,7 @@ class Changer
 	  			return @@change
 	  		end
 	  	end
-
 	end# end of class method
-
 	def self.calc_next_coin(money, coin_worth )
 		if money/coin_worth >= 1
 			num_coins = money/coin_worth
@@ -30,5 +20,4 @@ class Changer
 			@@money = money - coin_worth*num_coins
 		end
 	end
-
 end
